7ec0f6c8668c6b955d927071ee82e484671f6683
Nabeel Ahmad <nabeel.ahmad@frizhub.com>
2023-12-02 21:20:54 +0500

    development : setting up the env and configuring the postgres + orm config file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,33 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { TasksModule } from "./tasks/tasks.module";
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from "./auth/auth.module";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  imports: [TasksModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        type: "postgres",
+        host: config.get("DB_HOST"),
+        port: config.get("DB_PORT"),
+        username: config.get("DB_USER"),
+        password: config.get("DB_PASSWORD"),
+        database: config.get("DB_DATABASE"),
+        entities: [__dirname + "/**/*.entity.{js,ts}"],
+        synchronize: true,
+        logging: false,
+      }),
+      inject: [ConfigService],
+    }),
+    TasksModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
