e5916bf3ead8bae4e44b0bcbb8cdeb0832e661bc
Nabeel Ahmad <nabeel.ahmad@frizhub.com>
2023-12-02 20:02:08 +0500

    development : removing users and adding auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { TasksModule } from './tasks/tasks.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { TasksModule } from "./tasks/tasks.module";
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, TasksModule],
+  imports: [TasksModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
